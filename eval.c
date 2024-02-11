6d085068ba9dfb27735c37c86bf9efa86146e4e5
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-02-10 18:05:23 -0800

    Basic logic for eval printing

--- eval.c
+++ eval.c
@@ -68,11 +68,20 @@ void reverse(char *str) {
 void print_output_in_base (uint32_t result, int base, int width) {
 	int i=0;
 	char output[64];
+	if(base==2){
+		uint32_t mask;
+		if(width==32){
+			mask = (1 << (width-1)) -1;
+		} else{
+			mask = (1 << width) -1;
+		}
+		result = result & mask;
+	}
+	if(base==16){
+			uint32_t mask = (1 << width/4) -1;
+			result = result & mask;
+	}
 
-	uint32_t mask = (width << 1) -1;
-
-	result = result & mask;
-	
 	while(result!=0){
 		uint32_t temp = result % base;
 		if(temp<10){
@@ -82,6 +91,7 @@ void print_output_in_base (uint32_t result, int base, int width) {
         }
         result = result / base;
 	}
+	
 	int limit = width;
 	if (base==16) {
 		limit = limit / 4;
@@ -104,6 +114,95 @@ void print_output_in_base (uint32_t result, int base, int width) {
 	printf("%s\n", output);
 }
 
+int generate_output(uint32_t result, char* output, int base, int width){
+	int i = 0;
+	int denom;
+	if(base==2){
+		denom = 1;
+	} else {
+		denom = 4;
+	}
+	while(i<(width/denom)){
+		uint32_t temp = result % base;
+		output[i++] = uint32_digit_char(temp);
+   	 	result = result / base;
+	}
+
+	return i;
+	
+}
+
+
+void print_output_in_base2 (uint32_t result, int width) {
+	char output[64];
+	uint32_t mask;
+	if(width==32){
+		mask = (1 << (width-1)) -1;
+	} else{
+		mask = (1 << width) -1;
+	}
+	result = result & mask;
+
+	int i = generate_output(result, output, 2, width);
+	// while(i<width){
+		// output[i++] = '0';
+	// }
+	output[i++] = 'b';
+	output[i++] = '0';
+	output[i] = '\0';
+	reverse(output);
+	printf("%s\n", output);
+}
+
+void print_output_in_base10 (uint32_t result, int width) {
+	char output[64];
+	uint32_t mask;
+	if(width==32){
+		mask = (1 << (width-1)) -1;
+	} else{
+		mask = (1 << width) -1;
+	}
+	// printf("Result before mask %d\n", result);
+	result = result & mask;
+	// printf("Result %d\n", result);
+	// printf("Mask %d\n", mask);
+	// int i = generate_output(result, output, 10, width);
+	int i = 0;
+	int base = 10;
+	if(result==0){
+		output[i++] = '0';
+	}
+	while(result!=0){
+		uint32_t temp = result % base;
+		output[i++] = uint32_digit_char(temp);
+   	 	result = result / base;
+	}
+	output[i] = '\0';
+	reverse(output);
+	printf("%s\n", output);
+}
+
+void print_output_in_base16 (uint32_t result, int width) {
+	char output[64];
+	uint32_t mask;
+	if(width==32){
+		mask = (1 << (width-1)) -1;
+	} else{
+		 mask = (1 << width) -1;
+	}
+	result = result & mask;
+	int i = generate_output(result, output, 16, width);
+	// while(i<width/4){
+		// output[i++] = '0';
+	// }
+	output[i++] = 'x';
+	output[i++] = '0';
+	output[i] = '\0';
+	reverse(output);
+	printf("%s\n", output);
+}
+
+
 void eval_print(struct config_st *cp, uint32_t value) {
     /*
      * Handle -b -w -u
@@ -113,8 +212,14 @@ void eval_print(struct config_st *cp, uint32_t value) {
      // printf("Base  %d\n", cp->base);
      // printf("Width %d \n", cp->width);
      // printf("Unsigned flag %d\n", cp->unsigned_flag);
-    if(cp->base==10){
+    if(cp->base==10 && cp->unsigned_flag==true){
     	printf("%d\n", value);
+    } else if(cp->base==10) {
+      	print_output_in_base10(value, cp->width);
+    } else if(cp->base==2) {
+    	print_output_in_base2(value, cp->width);
+    } else if(cp->base==16) {
+       	print_output_in_base16(value, cp->width);
     } else{
    		print_output_in_base(value, cp->base, cp->width);
     }
